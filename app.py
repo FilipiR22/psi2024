87c7026918577e8bf9a0f5682f403ee1e25712df
Filipi Cruz <116102643+FilipiR22@users.noreply.github.com>
2024-03-05 13:04:36 +0000

    passando paRâmetros e variáveis para o html

--- app.py
+++ app.py
@@ -7,12 +7,24 @@ def index():
 
 @app.route('/compras')
 def compras():
-    return render_template('compras.html')
+    return render_template('compras.html', novoitem='cuzscuz',item2='melancia')
 
 @app.route('/mercados')
-def compras():
+def mercados():
     return render_template('mercados.html')
+
+@app.route('/gastos',defaults={'mes':'janeiro','gasto':0})
+@app.route('/gastos/<mes>',defaults={'gasto':0})
+@app.route('/gastos/<gasto>',defaults={'mes':'janeiro'})
+@app.route('/gastos/<mes>/<gasto>')
+def gastos(mes,gasto):
+    return render_template('gastos.html',mes=mes,gasto=gasto)
+
+@app.route('/dobro/<int:n>')
+def dobro(n):
+    resultado = n*2
+    return render_template('/dobro.html',n=n,resultado=resultado)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
